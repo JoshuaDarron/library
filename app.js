24c614aa8e48605fdea5ee50f9b93b6bee9b745e
Joshua Spears <j.dspears@yahoo.com>
2019-12-23 16:26:36 -0800

    Fixing up middleware

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(logger('dev'))
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    )
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    )
+    next()
+})
+
 app.use('/api', apiRoutes)
 app.use('*', express.static(path.join(__dirname, 'public')))
 
